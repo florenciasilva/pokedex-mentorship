60dc84b4ba6ade6408f875f30354287d2305b091
Florencia Silva <florencia.silva@dowjones.com>
2021-06-24 17:02:23 -0500

    adds initial boilerplate

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import { Main, Heading, Paragraph } from 'grommet'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Main pad="large">
+      <Heading>Something</Heading>
+      <Paragraph>Something about something</Paragraph>
+    </Main>
   );
 }
 
